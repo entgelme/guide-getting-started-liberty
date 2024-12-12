0204a460198ca9ebe0184cbf8437597e0e787383
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-12-12 09:54:15 +0100

    extended deployment def

--- prep_deploy.sh
+++ prep_deploy.sh
@@ -3,6 +3,12 @@
 export LIBERTYPORT=9080
 export LIBERTYIMG=liberty-on-container
 
+# Create default route to local image registry
+oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
+# Create trusted-ca ConfigMap, mirrored from the cluster wide one
+oc apply -f cm-trusted-ca.yml
+
+# Create the deployment for the app, which calls the remote service
 oc apply -f guide-getting-started-liberty-deployment.yml
 oc expose deployment/$LIBERTYIMG --port LIBERTYPORT --target-port LIBERTYPORT
 oc expose svc/$LIBERTYIMG
